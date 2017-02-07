e25dc12a64e55df76978abbe897461fef166dbcc
anies <jombo95@gmail.com>
2017-02-08 00:02:29 +0300

    верстка
    хедер,слайдер

--- PlatinumTravel/PlatinumTravel/App_Start/BundleConfig.cs
+++ PlatinumTravel/PlatinumTravel/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace PlatinumTravel
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/AdminScripts").Include(
                         "~/Scripts/AdminPageScript.js"));
